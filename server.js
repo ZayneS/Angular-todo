ea9c4c9e105148083d7cbdd553a3fd85a1aa8631
Zheng Shen <zheng@hillfineday-lm.gateway.pace.com>
2014-10-22 01:56:05 -0700

    set up server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+// server.js
+
+// set up =======================
+	var express = require('express');
+	var app = express();
+	var mongoose = require('mongoose');
+	var morgan = require('morgan');
+	var bodyParser = require('body-parser');
+	var methodOverride = require('method-override');
+
+// configuration ================
+
+	mongoose.connect('mongodb://zheng:1987911@novus.modulusmongo.net:27017/hiQ5adub');
+	app.use(express.static(__dirname + '/public'));
+	app.use(morgan('dev'));
+	app.use(bodyParser.urlencoded({'extended':'true'}));
+	app.use(bodyParser.json());
+	app.use(bodyParser.json({type: 'application/vnd.api+json'}));
+	app.use(methodOverride());
+
+
+	// listen (start app with node server.js) ======================================
+	app.listen(8080);
+	console.log("App listening on port 8080");
+
